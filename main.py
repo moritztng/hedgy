f35243af8fa3519690fd9ac9de77e8da043232ae
Moritz Thüning <moritz.thuening@gmail.com>
2021-08-26 14:27:05 +0200

    group ranking

--- main.py
+++ main.py
@@ -26,7 +26,14 @@ def rank(transcripts, query):
         rank = len(words & keywords)
         if rank:
           ranking.append(transcript[:2] + [time_stamp, text, rank])
-    return sorted(ranking, key=lambda x: x[-1], reverse=True)
+    ranking.sort(key=lambda x: x[-1], reverse=True)
+    ranking_grouped = []
+    for video_name, video_id, time_stamp, text, _ in ranking:
+      if ranking_grouped and ranking_grouped[-1][1] == video_id:  
+        ranking_grouped[-1][-1].append([time_stamp, text]) 
+      else:
+        ranking_grouped.append([video_name, video_id, [[time_stamp, text]]])
+    return ranking_grouped
 
 with open(join(dirname(abspath(__file__)), 'transcripts.p'), 'rb') as f:
     transcripts = load(f)
@@ -39,6 +46,8 @@ def hedgy(request):
                   </form>"""
     if 'query' in request.args:
         ranking = rank(transcripts, request.args.get('query'))
-        for video_name, video_id, time_stamp, text, _ in ranking:
-            response += f"<a href='https://youtu.be/{video_id}?t={time_stamp}'>{video_name} {timedelta(seconds=time_stamp)} {text}</a><br>\n"
+        for video_name, video_id, time_stamps in ranking:
+            response += f"<p>{video_name}</p><br>\n"
+            for time_stamp, text in time_stamps:
+                response += f"<a href='https://youtu.be/{video_id}?t={time_stamp}' style='text-decoration:none;'>{timedelta(seconds=time_stamp)} {text}</a><br>\n"
     return response
