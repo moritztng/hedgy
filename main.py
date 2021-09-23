d1309f950d3009dd34d7afa69505b1693dde86c4
Moritz Thüning <moritz.thuening@gmail.com>
2021-09-23 15:17:58 +0200

    remove unsimilar results

--- main.py
+++ main.py
@@ -14,9 +14,14 @@ tfidf_matrix = load_npz(join(hedgy_path, 'tfidf.npz'))
 similarity_matrix = np.load(join(hedgy_path, 'similarity.npy'))
 
 def hedgy(request):
-    ranking = []
+    ranking, sliced = [], False
     if 'query' in request.args and 'max' in request.args:
         query_vector = vectorizer.transform([request.args.get('query')])
-        similarity_vector = (tfidf_matrix @ query_vector.T).toarray().squeeze()
-        ranking = np.argsort(similarity_vector)[::-1][:int(request.args.get('max'))].tolist()
-    return render_template('hedgy.html', chapters=chapters, ranking=ranking, topics=topics, args=request.args)
+        if np.any(query_vector):
+            similarity_vector = (tfidf_matrix @ query_vector.T).toarray().squeeze()
+            max_chapters, max_request = np.count_nonzero(similarity_vector), int(request.args.get('max')))
+            if max_request < max_chapters:
+                max_chapters = max_request
+                sliced = True
+            ranking = np.argsort(similarity_vector)[::-1][:max_chapters].tolist()
+    return render_template('hedgy.html', chapters=chapters, ranking=ranking, sliced=sliced, topics=topics, args=request.args)
