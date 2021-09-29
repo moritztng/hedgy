72100659a125799b4d189a82bdf067277b544161
Moritz Thüning <moritz.thuening@gmail.com>
2021-09-29 10:46:07 +0200

    add 'Show more' to homepage

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 from preprocess import Vectorizer
 from flask import render_template
 from os.path import join, abspath, dirname
+from random import randint
 from pickle import load
 from scipy.sparse import load_npz
 
@@ -13,19 +14,27 @@ tfidf_matrix = load_npz(join(hedgy_path, 'tfidf.npz'))
 similarity_matrix = np.load(join(hedgy_path, 'similarity.npy'))
 
 def hedgy(request):
-    ranking, sliced = [], False
-    if 'max' in request.args and ('query' in request.args or 'similar' in request.args):
-        if 'query' in request.args:
-            query_vector = vectorizer.transform([request.args.get('query')])
-            similarity_vector = (tfidf_matrix @ query_vector.T).toarray().squeeze()
-        else:
-            similarity_vector = similarity_matrix[int(request.args.get('similar'))]
-        if np.any(similarity_vector):
-            max_chapters, max_request = np.count_nonzero(similarity_vector), int(request.args.get('max'))
-            if max_request < max_chapters:
-                max_chapters = max_request
-                sliced = True
-            ranking = np.argsort(similarity_vector)[::-1][:max_chapters].tolist()
+    ranking, sliced, max_request, seed = [], False, 50, None
+    if 'max' in request.args:
+        max_request = int(request.args.get('max'))
+        if 'query' in request.args or 'similar' in request.args:
+            if 'query' in request.args:
+                query_vector = vectorizer.transform([request.args.get('query')])
+                similarity_vector = (tfidf_matrix @ query_vector.T).toarray().squeeze()
+            else:
+                similarity_vector = similarity_matrix[int(request.args.get('similar'))]
+            if np.any(similarity_vector):
+                max_chapters = np.count_nonzero(similarity_vector)
+                if max_request < max_chapters:
+                    max_chapters = max_request
+                    sliced = True
+                ranking = np.argsort(similarity_vector)[::-1][:max_chapters].tolist()
+        elif 'seed' in request.args:
+            seed = int(request.args.get('seed'))
     else:
-        ranking = np.random.permutation(len(chapters))[:50].tolist()
-    return render_template('hedgy.html', chapters=chapters, ranking=ranking, sliced=sliced, args=request.args)
+        seed = randint(1, 1000000)
+    if seed:
+        np.random.seed(seed)
+        ranking = np.random.permutation(len(chapters))[:max_request].tolist()
+        sliced = True
+    return render_template('hedgy.html', chapters=chapters, ranking=ranking, sliced=sliced, max_request=max_request, seed=seed, args=request.args)
